7e41f2d4a488e3fb3d13e19b379a1b7b4c6c3367
murph <murphyl@outlook.com>
2022-12-04 18:27:51 +0800

    个人首页

--- src/core/app.v1.jsx
+++ src/core/app.v1.jsx
@@ -1,3 +1,23 @@
+import React from 'react';
+import { RecoilRoot } from 'recoil';
+import { HashRouter, Routes, Route } from "react-router-dom";
+
+import './app.v1.css';
+
+import BlogList from '../view/blog';
+
 export default function App() {
-    return 'x';
+    return (
+        <RecoilRoot>
+            <React.Suspense fallback="loading...">
+                <HashRouter>
+                    <Routes>
+                        <Route path="/" element="Home" />
+                        <Route path="/blog" element={<BlogList />} />
+                        <Route path="*" element="404" />
+                    </Routes>
+                </HashRouter>
+            </React.Suspense>
+        </RecoilRoot>
+    );
 }
